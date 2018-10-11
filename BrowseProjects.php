6b9cf261ab34882ee33185bd0a5cc0773deb00e8
HeHaowei <35795685+HeHaowei@users.noreply.github.com>
2018-10-11 14:03:29 +0800

    Revert "update BrowseProjects (#1)"
    This reverts commit c35845d4e21cd86f625259f3ef10ce35f21862b1.

--- BrowseProjects.php
+++ BrowseProjects.php
@@ -1,20 +1,11 @@
-<!DOCTYPE html>
-<head>
-    <title>UPDATE PostgreSQL data with PHP</title>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <style>li {list-style: none;}</style>
-</head>
-<body>
-<h2>Our current on-going projects</h2>
-<ul>
-    <form name="display" action="BrowseProjects.php" method="POST" >
-    </form>
-</ul>
 
+<body>
+
+	<h2>Our current on-going projects</h2>
 
 	<?php
   	// Connect to the database. Please change the password in the following line accordingly
-    $db       = pg_connect("host=localhost port=5432 dbname=postgres user=postgres password=test");
+    $db       = pg_connect("host=localhost port=5432 dbname=postgres user=postgres password='cs2102group10'");
     $funded   = pg_query($db, 'SELECT Project_id, title, category, total_amount FROM publish_projects WHERE current_amount >= total_amount');
     $unfunded = pg_query($db, 'SELECT Project_id, title, category, total_amount, total_amount - current_amount AS shortage FROM publish_projects WHERE current_amount < total_amount ORDER BY shortage')
 
@@ -25,7 +16,7 @@
 
     $number_funded =pg_query($db, 'SELECT COUNT(*) FROM publish_projects WHERE current_amount >=total_amount');
     $number_unfunded = pg_query($db, 'SELECT COUNT(*) FROM publish_projects WHERE current_amount<total_amount');
-    $average_shortage = pg_query($db, 'SELECT AVG (total_amount - current_amount) FROM publish_projects WHERE current_amount<total_amount');
+    $average_shortage = pg_query($db, 'SELECT AVERAGE(total_amount - current_amount) FROM publish_projects WHERE current_amount<total_amount');
 
     $funded =  pg_fetch_all($funded);
     $unfunded = pg_fetch_all($unfunded);
@@ -42,7 +33,6 @@
     ?>
  
  </body>
-</html>
 
 
 
